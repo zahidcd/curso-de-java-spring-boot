75a4b7f4851bb7e3ae0a5093619d5d1ef35db777
zahidcd <zahidmartin@hotmail.com>
2025-09-08 22:24:44 -0500

    Crear proyecto

---
+++ src/main/java/com/zahidcd/spring/app1/springboot_app1/SpringbootApp1Application.java
@@ -0,0 +1,13 @@
+package com.zahidcd.spring.app1.springboot_app1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/zahidcd/spring/app1/springboot_app1/SpringbootApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zahidcd.spring.app1.springboot_app1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
